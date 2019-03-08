8ed90421e22bd3d374f9c78fe205d80339fd396c
Mjay <martin_jarrin@hotmail.com>
2019-03-07 22:22:02 -0500

    Updates

---
+++ precache-manifest.72fd10f55a0efa041a90990f45867bbd.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "68902c8fdb5cff875dafb1d87dc2ffe5",
+    "url": "/eventsForm/static/media/fire.68902c8f.png"
+  },
+  {
+    "revision": "aa24485107146d1d4c57e65dc2440c57",
+    "url": "/eventsForm/static/media/cover.aa244851.jpg"
+  },
+  {
+    "revision": "3a1ee681b716cd63fbb2",
+    "url": "/eventsForm/static/js/runtime~main.3a1ee681.js"
+  },
+  {
+    "revision": "85722e467accbf676adb",
+    "url": "/eventsForm/static/js/main.85722e46.chunk.js"
+  },
+  {
+    "revision": "2b299224a41b3e1c4f47",
+    "url": "/eventsForm/static/js/2.2b299224.chunk.js"
+  },
+  {
+    "revision": "85722e467accbf676adb",
+    "url": "/eventsForm/static/css/main.76a4fd9a.chunk.css"
+  },
+  {
+    "revision": "2b299224a41b3e1c4f47",
+    "url": "/eventsForm/static/css/2.e1bc82a5.chunk.css"
+  },
+  {
+    "revision": "f44e838c530d7c2c2c2bff7fcd9dd7d0",
+    "url": "/eventsForm/index.html"
+  }
+];
